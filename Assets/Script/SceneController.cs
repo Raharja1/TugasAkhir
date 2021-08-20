bb33c4e514a2c5b76094e20de154c654f61e8657
Raharja1 <89232792+Raharja1@users.noreply.github.com>
2021-08-20 09:22:17 +0700

    Update files unity

---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void SceneChanger(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
